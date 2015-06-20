9ca8f8ad9fb79f5c4cb0f345018dab3baf0a0b88
itzik <itzikc2.ic@gmail.com>
2015-06-20 19:54:54 +0300

    dal

--- TheWeb/TheWeb/Models/project.cs
+++ TheWeb/TheWeb/Models/project.cs
@@ -2,12 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
 
 namespace TheWeb.Models
 {
     public class project
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Key]
         public int IDproject { get; set; }
+        public int NameProject { get; set; }
+        public DateTime deadlineToFinish { get; set; }
+        public string local { get; set; }
+        public string AboutTheProject { get; set; }
+        public string emailContact { get; set; }
+
 
     }
 }
--- TheWeb/TheWeb/Models/student.cs
+++ TheWeb/TheWeb/Models/student.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +9,13 @@ namespace TheWeb.Models
 {
     public class student
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Key]
         public int IDstudent { get; set; }
         public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime Bebirthday { get; set; }
 
     }
 }
